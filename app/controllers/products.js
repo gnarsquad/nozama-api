b87ab242361ded31af602ce9efe7544f4772dc01
MichaelB167 <michael.m.bates@gmail.com>
2016-05-13 16:24:40 -0400

    Add index function for products controller

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -4,6 +4,16 @@ const controller = require('lib/wiring/controller');
 const models = require('app/models');
 const Product = models.products;
 
+const authenticate = require('./concerns/authenticate');
+
+const index = (req, res, next) => {
+  Product.find()
+    .then(products => res.json({ products }))
+    .catch(err => next(err));
+};
+
 module.exports = controller({
-  
-})
+  index,
+}, { before: [
+  { method: authenticate, except: ['index', 'show'] },
+], });
