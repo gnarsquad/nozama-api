4cb36c99a5e16908505c3e5efec704ab102b7d05
MichaelB167 <michael.m.bates@gmail.com>
2016-05-13 15:56:05 -0400

    Scaffold product model

---
+++ app/models/product.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
