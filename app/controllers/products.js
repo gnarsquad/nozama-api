25483e927c54edb7f43336bad2ea79cc1dbab343
MichaelB167 <michael.m.bates@gmail.com>
2016-05-13 16:21:51 -0400

    Scaffold products controller

---
+++ app/controllers/products.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const controller = require('lib/wiring/controller');
+const models = require('app/models');
+const Product = models.products;
+
+module.exports = controller({
+  
+})
