1fd7e6447ec293f30832571265937905127fe6be
MichaelB167 <michael.m.bates@gmail.com>
2016-05-13 14:53:59 -0400

    Replace all instances of 'express-template' in app directory with 'nozama-api'

--- app/middleware/error-handler.js
+++ app/middleware/error-handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const debug = require('debug')('express-template:error-handler');
+const debug = require('debug')('nozama-api:error-handler');
 
 const errorHandler = (err, req, res, next) => {
   //jshint unused:false
