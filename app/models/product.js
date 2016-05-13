49fd1b8812c73b494baa74b825759dd521d0833a
MichaelB167 <michael.m.bates@gmail.com>
2016-05-13 16:12:52 -0400

    Change price type to number to ensure compatibility with stripe

--- app/models/product.js
+++ app/models/product.js
@@ -3,15 +3,12 @@
 const mongoose = require('mongoose');
 require('mongoose-type-url');
 
-require('mongoose-currency').loadType(mongoose);
-let Currency = mongoose.Types.Currency;
-
 const productSchema = new mongoose.Schema({
   type: String,
   subtype: String,
   name: String,
   description: String,
-  price: { type: Currency, required: true, min: -20000, max: 50000 },
+  price: Number,
   stock: Number,
   image: {
       url: {type: mongoose.SchemaTypes.Url, required: true},
